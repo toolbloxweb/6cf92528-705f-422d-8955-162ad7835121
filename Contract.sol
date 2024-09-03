1337abcc0ff6fb6ad6bc083cb5a1d2f0ab42ab9e
toolbloxweb <147279729+toolbloxweb@users.noreply.github.com>
2024-09-03 17:59:48 +0300

    Smart-contract code update

--- Contract.sol
+++ Contract.sol
@@ -5,15 +5,17 @@
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@tool_blox/contracts/contracts/NonTransferrableERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@tool_blox/contracts/contracts/WorkflowBaseUpgradeable.sol";
 import "@tool_blox/contracts/contracts/utils/Validate.sol";
 /*
-	Toolblox smart-contract workflow: https://app.toolblox.net/summary/rddtor_profile_contract_v1
+	Toolblox smart-contract workflow: https://app.toolblox.net/summary/rddtor_profile_contract_v1_1
 	# Description
 	
 	This contract serves as the holder for decentralized social network profile. The profile is an soulbound upgradeable NFT (ERC721).
+	
 	 * It is upgradeable (OpenZeppelin Initializable) to be able to add new properties, states and state transitions in subsequent iterations of development - properties such as Rating, Friends, Bio and more.
 	 * It is an NFT (ERC721) to be able to verify and check data in NFT portals and wallets.
 	 * It is soulbound to the owner wallet address (non-transferrable).
@@ -22,14 +24,12 @@ import "@tool_blox/contracts/contracts/utils/Validate.sol";
 	
 	The *Name* property of the profile will contain a hashed version of the username. Hashing will take place off-chain.
 */
-contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrableERC721Upgradeable, WorkflowBaseUpgradeable{
+contract ProfileWorkflow  is Initializable, OwnableUpgradeable, AccessControlUpgradeable, NonTransferrableERC721Upgradeable, WorkflowBaseUpgradeable{
 	struct Profile {
 		uint id;
 		uint64 status;
 		address wallet;
-		string image;
 		string name;
-		string description;
 	}
 	mapping(uint => Profile) public items;
 	function _assertOrAssignWallet(Profile memory item) private view {
@@ -41,11 +41,35 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 		}
 		item.wallet = _msgSender();
 	}
+	bytes32 public constant PLATFORM_ROLE = keccak256("PLATFORM_ROLE");
+	function _assertOrAssignPlatform(Profile memory) private view {
+		_checkRole(PLATFORM_ROLE);
+	}
+	function addPlatform(address adr) public returns (address) {
+		grantRole(PLATFORM_ROLE, adr);
+		return adr;
+	}
+	string _image;
+	function getImage() public view returns (string memory) {
+		return _image;
+	}
+	function setImage(string memory image) public onlyOwner {
+		_image = image;
+	}
+	string _description;
+	function getDescription() public view returns (string memory) {
+		return _description;
+	}
+	function setDescription(string memory description) public onlyOwner {
+		_description = description;
+	}
 	function initialize(address _newOwner, string memory tokenName, string memory tokenSymbol) public initializer onlyInitializing {
 		__Ownable_init(_newOwner);
+		__AccessControl_init();
 		__NonTransferrableERC721_init();
 		__ERC721_init(tokenName, tokenSymbol);
 		__WorkflowBase_init();
+		_grantRole(DEFAULT_ADMIN_ROLE, _newOwner);
 	}
     struct AddressSlot {
         address value;
@@ -137,6 +161,38 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 		);
 		itemsByName[item.name] = id;
 	}
+    function _increaseBalance(address account, uint128 amount) internal virtual override(ERC721Upgradeable) {
+        super._increaseBalance(account, amount);
+    }
+	function _baseURI() internal view virtual override returns (string memory) {
+		return _baseUri;
+	}
+	function tokenURI(uint256 tokenId) public view override returns (string memory) {
+		if (bytes(_baseUri).length > 0)
+		{
+			return string.concat(_baseUri, Strings.toString(tokenId));
+		} else {
+			Profile memory item = getItem(tokenId);
+			string memory url = string.concat(string.concat("{\"name\": \"", item.name), "\"");
+			url = string.concat(url, string.concat(string.concat(", \"description\": \"", getDescription()), "\""));
+			if (bytes(getImage()).length != 0)
+			{
+				url = string.concat(string.concat(url, ", \"image\": \"https://", getImage()), ".ipfs.w3s.link\"");
+			}
+			url = string.concat(url, ", \"attributes\":[");
+			url = string.concat(string.concat(url, " { \"trait_type\" : \"Status\", \"value\" :"),  (item.status == 0 ? "\"Active\"}" : "\"\"}"));
+			url = string.concat(url, string.concat(string.concat(",  { \"trait_type\" : \"Wallet\", \"value\" : \"", Strings.toHexString(uint160(item.wallet), 20)), "\"}"));
+			url = string.concat(url, "]");
+			return string.concat("data:application/json;utf8,", string.concat(url, "}"));
+		}
+	}
+	string private _baseUri;
+	function setBaseURI(string memory baseUri) external {
+		_baseUri = baseUri;
+	}
+	function supportsInterface(bytes4 interfaceId) public view override(AccessControlUpgradeable,ERC721Upgradeable) returns (bool) {
+		return super.supportsInterface(interfaceId);
+	}
 	function getId(uint id) public view returns (uint){
 		return getItem(id).id;
 	}
@@ -146,26 +202,24 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 	function getWallet(uint id) public view returns (address){
 		return getItem(id).wallet;
 	}
-	function getImage(uint id) public view returns (string memory){
-		return getItem(id).image;
-	}
 	function getName(uint id) public view returns (string memory){
 		return getItem(id).name;
 	}
-	function getDescription(uint id) public view returns (string memory){
-		return getItem(id).description;
-	}
 /*
 	### Transition: 'Register profile'
+	#### Notes
+	
+	This method is called by the RDDTOR platform. It mints a profile NFT which will be owned by the wallet. In the future, to further decentralize, the access rights would be extended to the owner wallet.
 	This transition creates a new object and puts it into `Active` state.
 	
 	#### Transition Parameters
 	For this transition, the following parameters are required: 
 	
 	* `Name` (Text)
+	* `Wallet` (Address)
 	
 	#### Access Restrictions
-	Access is specifically restricted to the user with the address from the `Wallet` property. If `Wallet` property is not yet set then the method caller becomes the objects `Wallet`.
+	Access only granted if caller is in any of these roles: Platform.
 	
 	#### Checks and updates
 	The following checks are done before any changes take place:
@@ -175,16 +229,18 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 	The following properties will be updated on blockchain:
 	
 	* `Name` (String)
+	* `Wallet` (Address)
 */
-	function registerProfile(string calldata name) public returns (uint256) {
+	function registerProfile(string calldata name,address wallet) public returns (uint256) {
 		uint256 id = _getNextId();
 		Profile memory item;
 		item.id = id;
-		_assertOrAssignWallet(item);
+		require(hasRole(PLATFORM_ROLE, _msgSender()), "Access restricted to: platform");
 		require(Validate.notEmpty( name ) && Validate.onlyAlphaNumAndSpace( name ), "Invalid name");
 		_setItemIdByName(item, 0);
 		_setItemIdByWallet(item, 0);
 		item.name = name;
+		item.wallet = wallet;
 		item.status = 0;
 		items[id] = item;
 		address newOwner = getItemOwner(item);
@@ -196,6 +252,9 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 	}
 /*
 	### Transition: 'Change name'
+	#### Notes
+	
+	This method is called by the RDDTOR platform to change the name of a profile. In the future, to further decentralize, the access rights would be extended to the owner wallet.
 	This transition begins from `Active` and leads to the state `Active`.
 	
 	#### Transition Parameters
@@ -205,7 +264,7 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 	* `Name` (Text)
 	
 	#### Access Restrictions
-	Access is specifically restricted to the user with the address from the `Wallet` property. If `Wallet` property is not yet set then the method caller becomes the objects `Wallet`.
+	Access only granted if caller is in any of these roles: Platform.
 	
 	#### Checks and updates
 	The following checks are done before any changes take place:
@@ -219,11 +278,10 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 	function changeName(uint256 id,string calldata name) public returns (uint256) {
 		Profile memory item = getItem(id);
 		address oldOwner = getItemOwner(item);
-		_assertOrAssignWallet(item);
+		require(hasRole(PLATFORM_ROLE, _msgSender()), "Access restricted to: platform");
 		_assertStatus(item, 0);
 		require(Validate.notEmpty( name ) && Validate.onlyAlphaNumAndSpace( name ), "Invalid name");
 		_setItemIdByName(item, 0);
-		_setItemIdByWallet(item, 0);
 		item.name = name;
 		item.status = 0;
 		items[id] = item;
@@ -232,7 +290,6 @@ contract ProfileWorkflow  is Initializable, OwnableUpgradeable, NonTransferrable
 			_transfer(oldOwner, newOwner, id);
 		}
 		_setItemIdByName(item, id);
-		_setItemIdByWallet(item, id);
 		emit ItemUpdated(id, item.status);
 		return id;
 	}
